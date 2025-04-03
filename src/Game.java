af215224c23d37b8ddcc6f8eb2b9fc745b1f6a9f
Your Name <samoylovstv@gmail.com>
2025-04-03 18:35:01 +0300

    new changes

---
+++ src/Game.java
@@ -0,0 +1,208 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Game {
+    int countMistake = 0;
+
+    private void gameLoop() {
+        Word hangman = new Word();
+
+        String word = hangman.randomWord();
+
+        char[] wordChar = word.toCharArray();
+        char[] lastWord = new char[wordChar.length];
+
+        Arrays.fill(lastWord, '_');
+
+        int countWin = wordChar.length;
+
+        StringBuilder rightLetter = new StringBuilder();
+        StringBuilder wrongLetter = new StringBuilder();
+
+        hangmanPicture();//countMistake
+        String firstOutEmptyWord = new String(lastWord);
+        System.out.println(GameConstants.WORD_COMPLIT);
+        System.out.println(firstOutEmptyWord);
+
+        while (countMistake < 7 && countWin > 0) {
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine().toLowerCase();
+
+            char[] sch = input.toCharArray();
+
+            if (!input.matches("[а-яА-ЯёЁ]+") || input.isEmpty()) {
+
+                isNotCorrectInput( lastWord, rightLetter, wrongLetter);//countMistake,
+
+            } else {
+
+                boolean containsInput = word.contains(input) && !Arrays.toString(lastWord).contains(input)
+                        && !rightLetter.toString().contains(input)
+                        && !wrongLetter.toString().contains(input);
+
+                if (containsInput) {
+                    for (int i = 0; i < wordChar.length; i++) {
+                        if (wordChar[i] == sch[0]) {
+                            lastWord[i] = wordChar[i];
+                            countWin--;
+                        } else if (lastWord[i] == 0) {
+                            lastWord[i] = '_';
+                        }
+                    }
+                    rightLetter.append(sch[0]);
+                } else if (!word.contains(input) && !wrongLetter.toString().contains(input)
+                ) {
+                    countMistake++;
+                    wrongLetter.append(sch[0]);
+                } else if (rightLetter.toString().contains(input) || wrongLetter.toString().contains(input)) {
+                    System.out.println(GameConstants.LETTER_ALREADY_ENTERED);
+                }
+                rightAndWrongLetterOutput(lastWord, rightLetter, wrongLetter);//, countMistake
+            }
+
+        }
+        gameResultOutput( rightLetter, lastWord, word); //countMistake,
+
+    }
+
+    private void rightAndWrongLetterOutput(char[] lastWord, StringBuilder rightLetter,
+                                           StringBuilder wrongLetter) { //,int countMistake
+        hangmanPicture();//countMistake
+        String result = new String(lastWord);
+        System.out.println(result);
+        System.out.print(GameConstants.GUESSED_LETTERS + rightLetter + " ");
+        System.out.println(GameConstants.MISTAKES + wrongLetter + " ");
+    }
+
+
+    private void gameResultOutput( StringBuilder rightLetter, char[] lastWord,
+                                   String word) {
+        Game hangman = new Game();
+        if (countMistake >= 7 && rightLetter.toString().length() != lastWord.length) {
+            System.out.println();
+            System.out.println(GameConstants.LOSE_AND_MYSTERIOUS_WORD + word);
+            hangman.startOrFinish();
+        } else if (countMistake < 7) {
+            System.out.println();
+            System.out.println(GameConstants.WIN);
+            System.out.println(GameConstants.THE_MYSTERIOUS_WORD + word);
+            hangman.startOrFinish();
+        }
+    }
+
+    private void isNotCorrectInput( char[] lastWord, StringBuilder rightLetter,
+                                    StringBuilder wrongLetter) {
+        System.out.println(GameConstants.IS_NOT_CORRECT_INPUT);
+        hangmanPicture();//countMistake
+        String result = new String(lastWord);
+        System.out.println(result);
+        System.out.print(GameConstants.GUESSED_LETTERS + rightLetter + " ");
+        System.out.println(GameConstants.MISTAKES + wrongLetter + " ");
+    }
+
+    private void hangmanPicture() {//int countMistake
+
+        HangmanPictures[] pictures = HangmanPictures.values();
+        System.out.println(pictures[countMistake].getValue());
+
+    }
+
+    enum HangmanPictures {
+        ZERO("""
+                _______________
+                |             |
+                |             |
+                |             |
+                |             |
+                |_____________|"""),
+        ONE("""
+                _______________
+                |      |      |
+                |             |
+                |             |
+                |             |
+                |_____________|"""),
+        TWO("""
+                _______________
+                |      |      |
+                |     [oo]    |
+                |             |
+                |             |
+                |_____________|"""),
+        THREE("""
+                _______________
+                |      |      |
+                |     [oo]__  |
+                |             |
+                |             |
+                |_____________|"""),
+        FOUR("""
+                _______________
+                |      |      |
+                |   __[oo]__  |
+                |             |
+                |             |
+                |_____________|"""),
+        FIVE("""
+                _______________
+                |      |      |
+                |   __[oo]__  |
+                |     (~~)    |
+                |             |
+                |_____________|"""),
+        SIX("""
+                _______________
+                |      |      |
+                |   __[oo]__  |
+                |     (~~)    |
+                |     |       |
+                |_____________|"""),
+        SEVEN("""
+                _______________
+                |      |      |
+                |   __[oo]__  |
+                |     (~~)    |
+                |     |  |    |
+                |_____________|""");
+
+        private final String value;
+
+
+        HangmanPictures(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+    }
+
+    public void startOrFinish() {
+        Game hangman = new Game();
+        System.out.println();
+        System.out.println(GameConstants.GAME_HANGMAN);
+        System.out.println(GameConstants.START_OR_FINISH_INPUT);
+        Scanner sc = new Scanner(System.in);
+
+        boolean loop = true;
+        while (loop) {
+            String startOrFinish = sc.nextLine();
+            if (startOrFinish.equals("1")) {
+                hangman.gameLoop();
+                loop = false;
+            } else if (startOrFinish.equals("2")) {
+                loop = false;
+
+            } else if (!startOrFinish.isEmpty()) {
+                System.out.println(GameConstants.IS_NOT_CORRECT_INPUT_2);
+            } else {
+                System.out.print(GameConstants.EMPTY_INPUT);
+            }
+        }
+
+    }
+
+
+}
---
+++ src/Word.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Word {
+
+
+    public  String randomWord(){
+        File dictionary = new File("newnouns.txt");
+        Random random = new Random();
+        int randomNumber = random.nextInt(1000);
+
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(dictionary);
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка чтения файла"+e.getMessage());
+        }
+
+        Map<Integer, String> words = new HashMap<>();
+        int count = 0;
+
+        while (scanner!=null && scanner.hasNextLine()) {
+
+            words.put(count, scanner.nextLine());
+            count++;
+        }
+
+
+
+        return words.get(randomNumber);
+    }
+
+}
